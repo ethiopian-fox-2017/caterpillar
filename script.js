86da22aac11a6b2f2727e967098c0d0130e8cbeb
andrasat <andra.satria1@gmail.com>
2017-04-05 11:59:04 +0700

    added something to console log

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const lib = require('./lib/lib.js')
 
 // isEmpty
-console.log('-----')
+console.log('---ISEMPTY---')
 console.log(lib.isEmpty([]))
 console.log(lib.isEmpty(1))
 console.log(lib.isEmpty([1,2,3]))
 console.log('-----')
 // count
-console.log('-----')
+console.log('---COUNT---')
 console.log(lib.count([]));
 console.log(lib.count([1,2,3]))
 console.log('-----')
 // head
-console.log('-----')
+console.log('---HEAD---')
 console.log(lib.head([1,2,3]))
 console.log(lib.head([4,5,7]))
 console.log('-----')
 // tail
-console.log('-----')
+console.log('---TAIL---')
 console.log(lib.tail([1,2,3]))
 console.log(lib.tail([4,5,7]))
 console.log('-----')
 // flatten
-console.log('-----')
+console.log('---FLATTEN---')
 console.log(lib.flatten([1,[2,3],4,[5,[6,7]]]))
 console.log('-----')
