ba83a90deb66c006fa66465a4eaff2e48e4db34e
Anthony Juan <anthonyjuan95@gmail.com>
2017-04-05 12:39:06 +0700

    caterpillar done

---
+++ index.js
@@ -0,0 +1,10 @@
+var isEmpty = require('./lib/isEmpty');
+var count = require('./lib/count');
+var head = require('./lib/head');
+var tail = require('./lib/tail');
+var flatten = require('./lib/flatten')
+
+
+var array = [[0, 1], [2, 3], [4, 5]];
+console.log(flatten(array));
+
